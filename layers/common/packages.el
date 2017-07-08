04789597578daade382ef6a665476cb3ac7eded5
xkwu <xkwu1990@gmail.com>
2017-07-08 23:55:12 +0800

    add gtd

--- layers/common/packages.el
+++ layers/common/packages.el
@@ -81,12 +81,72 @@ Each entry is either:
   )
 (global-set-key (kbd "<f4>") 'open-spacemacsd-file)
 
-(setq my-warehouse-root "/Volumes/work/emacs")
+;; define the refile targets
+(defvar org-agenda-dir "/Volumes/work/emacs/agenda" "gtd org files location")
+(setq org-agenda-file-inbox (expand-file-name "inbox" org-agenda-dir))
+(setq org-agenda-file-note (expand-file-name "notes.org" org-agenda-dir))
+(setq org-agenda-file-gtd (expand-file-name "gtd.org" org-agenda-dir))
+(setq org-agenda-file-journal (expand-file-name "journal.org" org-agenda-dir))
+(setq org-agenda-file-code-snippet (expand-file-name "snippet.org" org-agenda-dir))
+(setq org-default-notes-file (expand-file-name "gtd.org" org-agenda-dir))
+(setq org-agenda-files (list org-agenda-file-gtd))
 
-;;projectile
-(setq projectile-project-root my-warehouse-root)
+(with-eval-after-load 'org-agenda
+  (define-key org-agenda-mode-map (kbd "P") 'org-pomodoro)
+  (spacemacs/set-leader-keys-for-major-mode 'org-agenda-mode
+    "." 'spacemacs/org-agenda-transient-state/body)
+  )
+;; the %i would copy the selected text into the template
+;;http://www.howardism.org/Technical/Emacs/journaling-org.html
+;;add multi-file journal
+(setq org-capture-templates
+      '(
+        ("i" "" entry (file+headline org-agenda-file-gtd "In box")
+         "* TODO [#B] %?\n  %i\n"
+         :empty-lines 1)
+        ("t" "Todo" entry (file+headline org-agenda-file-gtd "Workspace")
+         "* TODO [#B] %?\n  %i\n"
+         :empty-lines 1)
+        ("n" "notes" entry (file+headline org-agenda-file-note "Quick notes")
+         "* %?\n  %i\n %U"
+         :empty-lines 1)
+        ("b" "Blog Ideas" entry (file+headline org-agenda-file-note "Blog Ideas")
+         "* TODO [#B] %?\n  %i\n %U"
+         :empty-lines 1)
+        ("s" "Code Snippet" entry
+         (file org-agenda-file-code-snippet)
+         "* %?\t%^g\n#+BEGIN_SRC %^{language}\n\n#+END_SRC")
+        ("w" "work" entry (file+headline org-agenda-file-gtd "Cocos2D-X")
+         "* TODO [#A] %?\n  %i\n %U"
+         :empty-lines 1)
+        ("c" "Chrome" entry (file+headline org-agenda-file-note "Quick notes")
+         "* TODO [#C] %?\n %(zilongshanren/retrieve-chrome-current-tab-url)\n %i\n %U"
+         :empty-lines 1)
+        ("l" "links" entry (file+headline org-agenda-file-note "Quick notes")
+         "* TODO [#C] %?\n  %i\n %a \n %U"
+         :empty-lines 1)
+        ("j" "Journal Entry"
+         entry (file+datetree org-agenda-file-journal)
+         "* %?"
+         :empty-lines 1)))
 
+;;An entry without a cookie is treated just like priority ' B '.
+;;So when create new task, they are default 重要且紧急
+(setq org-agenda-custom-commands
+      '(
+        ("w" . "任务安排")
+        ("wa" "重要且紧急的任务" tags-todo "+PRIORITY=\"A\"")
+        ("wb" "重要且不紧急的任务" tags-todo "-Weekly-Monthly-Daily+PRIORITY=\"B\"")
+        ("wc" "不重要且紧急的任务" tags-todo "+PRIORITY=\"C\"")
+        ("b" "Blog" tags-todo "BLOG")
+        ("p" . "项目安排")
+        ("pw" tags-todo "PROJECT+WORK+CATEGORY=\"cocos2d-x\"")
+        ("pl" tags-todo "PROJECT+DREAM+CATEGORY=\"zilongshanren\"")
+        ("W" "Weekly Review"
+         ((stuck "") ;; review stuck projects as designated by org-stuck-projects
+          (tags-todo "PROJECT") ;; review all projects (assuming you use todo keywords to designate projects)
+          ))))
 
-;;agenda
-(setq org-agenda-files (list (concat my-warehouse-root "/agenda/")))
-
+(setq org-refile-targets '(("~/warehouse/gtd/gtd.org" :maxlevel . 3)
+                           ("~/warehouse/gtd/someday.org" :level . 1)
+                           ("~/warehouse/gtd/tickler.org" :maxlevel . 2)))
